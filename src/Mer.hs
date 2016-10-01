5deca701c13022ebdca705a531e944d6127192ff
Athan Clark <athan.clark@gmail.com>
2016-10-01 04:33:35 -0700

    oye

--- src/Mer.hs
+++ src/Mer.hs
@@ -3,10 +3,10 @@ module Main where
 
 import System.INotify
 import System.Environment (getArgs)
-import Data.IORef
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.STM
 import Control.Concurrent.STM.TSem
+import Control.Concurrent.STM.TVar
 import Control.Monad (forM_)
 
 
@@ -22,15 +22,23 @@ main = do
 
 
   debouncer <- atomically $ newTSem 0
+  notif <- initINotify
+  expectation <- newTVarIO (0 :: Int)
 
-  watcher <- withINotify $ \notif -> addWatch notif [Modify] file $ \e -> do
+  watcher <- addWatch notif [Modify] file $ \e -> do
+    e' <- atomically $ do
+      modifyTVar expectation (+1)
+      readTVar expectation
     print e
     threadDelay second
-    atomically $ signalTSem debouncer
+    e'' <- readTVarIO expectation
+    if e' == e''
+    then atomically $ signalTSem debouncer
+    else pure ()
 
   atomically $ waitTSem debouncer
   removeWatch watcher
-
+  killINotify notif
 
 
 second = 1000000
